0be6a37a9e28e70479be4525e7aed2e54eede05c
Aya Ahmed Algobashe <95775601+AyaAlgobashe@users.noreply.github.com>
2024-03-27 02:22:33 +0200

    Add files via upload

---
+++ lab-5/src/app/services/productList.ts
@@ -0,0 +1,14 @@
+import { IProduct } from '../models/iproduct';
+
+export let productList: IProduct[] = [
+  { id: 1, name: "ZZ Plant", price: 200,countaty:4,},
+  { id: 2, name: "Aurora", price: 200,countaty:0,},
+  { id: 3, name: "Echeveria Succulent", price: 200,countaty:8},
+  { id: 4, name: "Devil’s Ivy", price: 200,countaty:1},
+  
+  { id: 5, name: "ZZ Plant", price: 200,countaty:0},
+  { id: 6, name: "Aurora", price: 200,countaty:10},
+  { id: 7, name: "Echeveria Succulent", price: 200,countaty:1},
+  { id: 8, name: "Devil’s Ivy", price: 200,countaty:9
+},
+];
